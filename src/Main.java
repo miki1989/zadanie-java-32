3d1229f82c11373c97ecaae1e03ed866bb7c44bd
miki1989 <mikolajheller@gmail.com>
2018-07-05 10:23:50 +0200

    commit to latest task2

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Klasa to zbiór elementów, które przechowują informacje o obiektach i je przetwarzają");
+        System.out.println("Obiekt to reprezentacja klasy i jest tworzony na podstawie klasy");
+        System.out.println("Zmienna to przechowalnia różnego typu danych");
+        System.out.println("Każda zmienna musi mieć określony typ wartości, np. int, albo String.");
+        System.out.println("W klasie mogą się znajdować metody, konstruktory, klasy i obiekty");
+        System.out.println("Klasy są wywoływane przez operator new i służą do tworzenia nowych obiektów.");
+        System.out.println("Jeśli w klasie zdefiniowano chociaż jeden konstruktor to kompilator nie wygeneruje dla nas konstruktora domyślnego");
+        System.out.println();
+        System.out.println("car1 nazywamy referencją nowego obiektu");
+        System.out.println("car1 i car2 to dwie referencje wskazujące na ten sam obiekt");
+    }
+}
